1f04d6552677205cbf2a1cf01d7dc789c3215b30
Taewoon Kim <taewoon@humem.ai>
2025-02-07 21:03:21 +0100

    Make math work in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  site: 'https://odyssey-theme.sapling.supply/', 
+  sitemap: true,
+  integrations: [
+    sitemap(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex]
+    }),
+    lit(),
+    icon()
+  ]
 });
