bcc0687ef5ee71cb30850bb3c95e4b92aaa402c2
Taewoon Kim <taewoon@humem.ai>
2025-04-11 23:29:30 +0300

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `HumemAI | Your AI Assistant with Humean-Like Memory`, // The title of the website
-	description: `Building AI agents with humean-like memory to help you remember things better.`, // The description of the website
+	title: `HumemAI | Your AI Assistant with Human-Like Memory`, // The title of the website
+	description: `Building AI agents with human-like memory to help you remember things better.`, // The description of the website
 	url: `https://humem.ai`, // No trailing slash!
 	name: `HumemAI`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
