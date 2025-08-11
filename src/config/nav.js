025db526afbe35f4ee34125066a68bf6874c34b4
Taewoon Kim <taewoon@humem.ai>
2025-08-11 14:14:54 +0200

    add pricing and research pages; update navigation and team member emails

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,11 +1,19 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
+	// {
+	// 	title: 'Home',
+	// 	slug: '/',
+	// },
 	{
 		title: 'About',
 		slug: '/about',
+	},
+	{
+		title: 'Pricing',
+		slug: '/pricing',
+	},
+	{
+		title: 'Research',
+		slug: '/research',
 	},
 	{
 		title: 'Team',
