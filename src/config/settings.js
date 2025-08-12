6066033792215c7904999587ce4b14bcf387550e
Taewoon Kim <taewoon@humem.ai>
2025-08-12 14:19:17 +0200

    Refactor SEO metadata and image path settings for improved clarity and consistency across components

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,7 @@ export default {
 	description: `Building AI agents with human-like memory to help you remember things better.`, // The description of the website
 	url: `https://humem.ai`, // No trailing slash!
 	name: `HumemAI`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	socialImage: `/assets/images/landing.png`,
 	enableThemeSwitcher: true,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
